53d933bdaf7840519205a8d19e4e24a4b297e405
Joshua Carp <jm.carp@gmail.com>
2015-04-06 17:20:16 -0400

    Unbundle alabaster.

--- docs/conf.py
+++ docs/conf.py
@@ -3,10 +3,10 @@
 import os
 import sys
 
-sys.path.insert(0, os.path.abspath('..'))
-sys.path.insert(0, os.path.abspath('_themes'))
-import guardrail
 import alabaster
+
+sys.path.insert(0, os.path.abspath('..'))
+import guardrail
 
 
 extensions = [
